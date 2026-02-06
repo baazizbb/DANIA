ef806595068fe038392208d0f65b5eb8ef716cc3
baazizbb <35135820+baazizbb@users.noreply.github.com>
2026-02-06 12:59:33 +0100

    creation todolist

--- React-apps/myapp/src/App.tsx
+++ React-apps/myapp/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./pages/Home";  
 
@@ -11,7 +11,6 @@ function App() {
 
       <Home />
       
-
     </>
   );
 }
